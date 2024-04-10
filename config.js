44e86ea998434a6406cc06060d293f9d18d25d33
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2024-04-10 22:16:07 +0200

    Vercel

---
+++ config.js
@@ -0,0 +1,6 @@
+const dotenv = require("dotenv").config();
+
+const API_KEY = process.env?.NG_APP_API_KEY;
+const APP_TEST = process.env?.APP_TEST;
+console.log(`API_KEY :${API_KEY}`);
+console.log(`APP_TEST :${APP_TEST}`);
